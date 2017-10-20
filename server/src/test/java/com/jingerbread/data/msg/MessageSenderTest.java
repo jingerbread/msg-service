0472005f6206a54f6ecfdae0642ed11c49ea5b6c
Maria Fedorova <mfedorova@devexperts.com>
2017-10-20 16:48:23 +0300

    [fixed] client processing of invalid json messages, [fixed] failing test

--- server/src/test/java/com/jingerbread/data/msg/MessageSenderTest.java
+++ server/src/test/java/com/jingerbread/data/msg/MessageSenderTest.java
@@ -13,7 +13,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @Slf4j
 @RunWith(SpringRunner.class)
-@TestPropertySource(locations = "src/test/resources/test.properties")
+@TestPropertySource(properties = { "test.value=test" })
 @SpringBootTest
 public class MessageSenderTest {
 
